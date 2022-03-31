674b770246881b0fcab9e464b5d82d47551ec643
The Duong <daoduong.the@gmail.com>
2022-03-31 14:18:46 +0700

    Exam: TodoList - render Todo List - add events onClick - remove todoItem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import "./App.scss";
-import ColorBox from "./components/ColorBox";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "Todo" },
+    { id: 2, title: "Todo List" },
+    { id: 3, title: "Todo Item" },
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todoList.findIndex((x) => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      <h1>Ối dồi ôi ReactJS</h1>
+      <h1>ReactJS hook</h1>
 
-      <ColorBox />
+      {/* <ColorBox /> */}
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
