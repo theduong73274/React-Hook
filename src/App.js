df742df72e36a47704257d01ca57051b9d48e20a
The Duong <daoduong.the@gmail.com>
2022-04-12 14:05:57 +0700

    - add Handle call API - Render item PostList - Use UseEffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
+import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
@@ -10,6 +11,31 @@ function App() {
     { id: 3, title: "Todo Item" },
   ]);
 
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      //  ...
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const { data, pagination } = responseJSON;
+        // console.log(data);
+        // console.log(pagination);
+
+        setPostList(data);
+      } catch (error) {
+        console.log("Fail", error.message);
+      }
+    }
+
+    fetchPostList();
+  }, []);
+
+  // Handle Todo List Click
   function handleTodoClick(todo) {
     console.log(todo);
     const index = todoList.findIndex((x) => x.id === todo.id);
@@ -20,6 +46,7 @@ function App() {
     setTodoList(newTodoList);
   }
 
+  // Handle TodoList Submit
   function handleTodoFormSubmit(formValues) {
     // console.log(formValues);
     // add new todo to current todo list
@@ -34,11 +61,12 @@ function App() {
 
   return (
     <div className="App">
-      <h1>ReactJS hook</h1>
+      <h1>ReactJS hook - Post List</h1>
 
       {/* <ColorBox /> */}
-      <TodoForm onSubmit={handleTodoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+      <PostList posts={postList} />
     </div>
   );
 }
